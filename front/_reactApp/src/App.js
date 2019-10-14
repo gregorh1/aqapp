099da947f719bc79ee01182202e74d5b6b44c016
Grzegorz <grzegorz.hul@gmail.com>
2019-10-14 12:54:04 +0200

    - Leaflet configuration plus minor improves

--- front/_reactApp/src/App.js
+++ front/_reactApp/src/App.js
@@ -1,16 +1,58 @@
 import React, { Component } from 'react';
-
-// fetch('http://localhost:3000/') // works with cross-origin allowed
-// .then(resp => {
-//     console.log(resp)
-// })
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import GeoLocBtn from './components/GeoLocBtn'
+import SensorsMarkers from './components/SensorsMarkers'
 
 class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            lat: 51.919438,
+            lng: 19.145136,
+            zoom: 5
+        }
+    }
+    myGeoLocation = () => {
+        const onSuccess = (pos) => {
+            this.setState({
+                lat: pos.coords.latitude,
+                lng: pos.coords.longitude,
+                zoom: 13
+            });
+            this.refs.map.leafletElement.flyTo([pos.coords.latitude, pos.coords.longitude], 13)
+        }
+        const onError = (error) => {
+            this.setState({
+                lat: 50.3,
+                lng: 19.166667
+            })
+            console.error('code: ' + error.code + '\n' +
+                'message: ' + error.message + '\n');
+        }
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    }
+
+    componentDidMount() {
+    }
+
     render() {
+        const position = [this.state.lat, this.state.lng];
         return (
-        <div className="is-mainContainer">
-            ssdgsdg
-      </div>
+            <div>
+                <Map ref='map' center={position} zoom={this.state.zoom}>
+                    <TileLayer
+                        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={position}>
+                        <Popup>
+                            MOJA LOKALIZACJA
+                        </Popup>
+                    </Marker>
+                    <SensorsMarkers />
+                </Map>
+                <GeoLocBtn myGeoLocation={this.myGeoLocation} />
+            </div>
         );
     }
 }
